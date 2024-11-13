9640af98465661f769f0fabc0378e0f9454a962e
paulopc777 <62563753+paulopc777@users.noreply.github.com>
2024-11-13 15:45:45 -0300

    Update

--- monitor/main.go
+++ monitor/main.go
@@ -66,6 +66,15 @@ type ResultData struct {
 	PhoneNumber []SocialAccount `json:"phone_number"`
 }
 
+type ResultUserData struct {
+	Name   string `json:"name"`
+	Online int `json:"is_online"`
+}
+type MonitorUserData struct {
+	Name   string `json:"name"`
+	Online int `json:"is_online"`
+}
+
 func GetMonitorData(token string) ([]ResultData, error) {
 	req, err := http.NewRequest("GET", "https://api-v2.monitchat.com/api/v1/social/whatsapp/monitor", nil)
 	if err != nil {
@@ -104,6 +113,49 @@ func GetMonitorData(token string) ([]ResultData, error) {
 	return results, nil
 }
 
+func GetUserMonitor(token string) ([]ResultUserData, error) {
+	// Cria a requisição HTTP
+	req, err := http.NewRequest("GET", "https://api-v2.monitchat.com/api/v1/user/monitor?take=500&filter=[[%22active%22,%22%3D%22,1]]", nil)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao criar requisição: %v", err)
+	}
+
+	// Define o cabeçalho de autorização
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	// Envia a requisição
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao fazer requisição do monitor: %v", err)
+	}
+	defer res.Body.Close() // Garante que o corpo da resposta será fechado
+
+	// Lê o corpo da resposta
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("erro ao ler body da requisição: %v", err)
+	}
+
+	// Decodifica o JSON para a estrutura de resposta
+	var response struct {
+		Data []MonitorUserData `json:"data"`
+	}
+	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, fmt.Errorf("erro ao decodificar o JSON: %v", err)
+	}
+
+	// Transforma os dados para o formato desejado
+	var results []ResultUserData
+	for _, item := range response.Data {
+		results = append(results, ResultUserData{
+			Name:   item.Name,
+			Online: item.Online,
+		})
+	}
+
+	return results, nil
+}
+
 func HandleMetrics(token string) func(*gin.Context) {
 	return func(c *gin.Context) {
 
@@ -123,7 +175,19 @@ func HandleMetrics(token string) func(*gin.Context) {
 				)
 			}
 		}
+		dataOther, err := GetUserMonitor(token)
+		if err != nil {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("Erro ao obter dados de monitoramento de usuários: %v", err))
+			return
+		}
 
+		// Formata as métricas de usuário para Prometheus
+		for _, item := range dataOther {
+			metrics += fmt.Sprintf(
+				"monitchat_whatsapp_active_user{name=\"%s\", online=\"%d\"}\n",
+				item.Name, item.Online,
+			)
+		}
 		// Define o conteúdo como "text/plain" para o Prometheus
 		c.Data(http.StatusOK, "text/plain; charset=utf-8", []byte(metrics))
 	}
